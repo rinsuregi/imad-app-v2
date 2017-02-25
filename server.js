33092cb11b7d5e6eb68c61277297ad1897f527df
rinsuregi <rinsuregi@gmail.com>
2017-02-25 18:26:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter',function(req,res){
     counter =counter + 1;
-    app.send(counter.toString())
+    app.send(counter.toString());
 });
 
 app.get('/:articleName', function(req,res){
