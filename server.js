c129edb5d9b70b9bd14dca2be23992e45c689fbc
rinsuregi <rinsuregi@gmail.com>
2017-03-18 12:10:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/test-db', function (req,res){
       if(err){
           res.status(500).send(err.tostring());
       }else {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
       
   });
