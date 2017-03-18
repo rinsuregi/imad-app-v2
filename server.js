f4d14056d1d02174333818de8e12e74d6b437202
rinsuregi <rinsuregi@gmail.com>
2017-03-18 16:05:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
+var crypto = require('crypto');
 var config = {
   host: 'db.imad.hasura-app.io',
   port: '5432',
@@ -86,6 +87,18 @@ function createTemplate (data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input)
+{ var hashed = crypto.pbkdf2Sync(input, salt,10000,512,'sha512');
+  return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req, res ){
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
+
 var pool = new Pool(config);
 app.get('/test-db', function (req,res){
   pool.query('SELECT * FROM test', function(err, result){
